2c895ef584150cbbbaf6574945e767747c42b291
rijicu <rijicu@mail.ru>
2017-10-04 07:42:19 +0300

    ClassWork1

--- IsElementsPresentOnThePage.java
+++ IsElementsPresentOnThePage.java
@@ -9,29 +9,24 @@ import java.util.concurrent.TimeUnit;
 
 public class IsElementsPresentOnThePage {
 
+
+
     @Test
     public void isElementsPresentOnThePage() {
-        WebDriver driver = new ChromeDriver();
+        //WebDriver driver = new ChromeDriver();
+        MainPage mainPage = new MainPage(driver);
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
         driver.get("http://localhost/");
-        WebElement myStore = driver.findElement(By.xpath("//*[@id=\"header\"]/a/img"));
-        myStore.isDisplayed();
-        WebElement userRegion = driver.findElement(By.id("region"));
-        userRegion.isDisplayed();
-        WebElement itemInCart = driver.findElement(By.className("quantity"));
-        itemInCart.isDisplayed();
-        WebElement searchFild = driver.findElement(By.name("query"));
-        searchFild.isDisplayed();
-        WebElement categoryList = driver.findElement(By.id("box-category-tree"));
-        categoryList.isDisplayed();
-        WebElement userLoginFild = driver.findElement(By.name("email"));
-        userLoginFild.isDisplayed();
-        WebElement userPassFild = driver.findElement(By.name("password"));
-        userPassFild.isDisplayed();
-        WebElement loginButton = driver.findElement(By.name("login"));
-        loginButton.isDisplayed();
 
 
+        mainPage.myStore.isDisplayed();
+        mainPage.userRegion.isDisplayed();
+        mainPage.itemInCart.isDisplayed();
+        mainPage.searchFild.isDisplayed();
+        mainPage.categoryList.isDisplayed();
+        mainPage.userLoginFild.isDisplayed();
+        mainPage.userPassFild.isDisplayed();
+        mainPage.loginButton.isDisplayed();
     }
 }
---
+++ MainPage.java
@@ -0,0 +1,26 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.FindBy;
+
+public class MainPage {
+    WebDriver driver = new ChromeDriver();
+ // WebDriver driver;
+
+    WebElement myStore = driver.findElement(By.xpath("//*[@id=\"header\"]/a/img"));
+/*    @FindBy(xpath = "//*[@id=\"header\"]/a/img")
+            WebElement myStore;*/
+    WebElement userRegion = driver.findElement(By.id("region"));
+    WebElement itemInCart = driver.findElement(By.className("quantity"));
+    WebElement searchFild = driver.findElement(By.name("query"));
+    WebElement categoryList = driver.findElement(By.id("box-category-tree"));
+    WebElement userLoginFild = driver.findElement(By.name("email"));
+    WebElement userPassFild = driver.findElement(By.name("password"));
+    WebElement loginButton = driver.findElement(By.name("login"));
+
+    MainPage(WebDriver driver){
+        this.driver = driver;
+    }
+
+}
